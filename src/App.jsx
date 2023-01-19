b3755b127780e41532c0adce46e944b8a144a4a7
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-01-19 20:39:27 +0400

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Aside from './components/Aside/Aside';
-import Header from './components/Header/Header';
 import Counter from './pages/Counter/Counter';
 import Home from './pages/Home/Home';
 import './reset.scss';
